f5e62b102f78cbce0a69c62b301bf5e612b909e5
minung Han <hmu332233@likelion.org>
2017-05-22 18:18:51 +0000

    랭킹, 실시간 리뷰 추가

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,5 +1,11 @@
 class PageController < ApplicationController
   def index
     @tents = Tent.all
+    
+    @lanks = Tent.order(likes: :desc).limit(5)
+    
+    @reviews = Review.order(id: :desc).limit(5)
+    
+    ap @reviews
   end
 end
